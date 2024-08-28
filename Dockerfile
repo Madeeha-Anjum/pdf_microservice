bda18582725d2417b83f4edaa8f60ae235c4fdfa
Madeeha-Anjum <maadeeeehaa@gmail.com>
2024-08-27 22:53:07 -0600

    added weasyprint

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,12 @@
 # Debeing from the official Python image
 FROM python:3.12-slim  
 
+# open scripot to install weasyprint dependencies
+RUN apt-get update
+# install weasyprint dependencies
+RUN apt install -y python3-pip libpango-1.0-0 libpangoft2-1.0-0 libharfbuzz-subset0 libjpeg-dev libopenjp2-7-dev libffi-dev 
+
+
 WORKDIR /app
 
 RUN pip install pdm
--- src/micro/main.py
+++ src/micro/main.py
@@ -1,7 +1,9 @@
+import io
 from typing import Union
-
 from fastapi import FastAPI
+from fastapi.responses import FileResponse
 from pydantic import BaseModel
+from weasyprint import HTML
 
 app = FastAPI()
 
@@ -12,15 +14,18 @@ class Item(BaseModel):
     is_offer: Union[bool, None] = None
 
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
-
-
+@app.get("/", response_class=FileResponse)
+def read_root() -> FileResponse:
+    pdf = HTML(string="<h1>Hello World</h1>").write_pdf("hello-world.pdf")
+    bytes = io.BytesIO(pdf)
+    bytes.seek(0)
+    return FileResponse("hello-world.pdf", media_type="application/pdf", filename="hello-world.pdf")
+    
+    
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
-
+ 
 
 @app.put("/items/{item_id}")
 def update_item(item_id: int, item: Item):
